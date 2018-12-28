6c5ca6187de3a1e853cfd4ccb250f1b308a74f1f
FF879165436 <879165436@qq.com>
2018-12-28 23:28:41 +0800

    项目搜索栏静态布局完完成

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,25 @@ const rename = require('gulp-rename');
 gulp.task('js',function(){
 	gulp.src('src/js/*.js').pipe(concat('index.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist/js'));
 })
 gulp.task('sass',function(){
 	gulp.src('./src/sass/*.scss')
 	.pipe(sass())
+	.pipe(cssnano())
 	.pipe(rename({"suffix" : ".min"}))
 	.pipe(gulp.dest('./dist/css'));
 })
+gulp.task('img',function(){
+	gulp.src('src/img/*.*')
+	.pipe(rename({"suffix" : ".min"}))
+	.pipe(imagemin())
+	.pipe(gulp.dest('./dist/img'));
+})
+//监听
 gulp.task('default',function(){
 	gulp.watch('./src/js/*.js',['js']);
 	gulp.watch('./src/sass/*.scss',['sass']);
+	gulp.watch('./src/img/*.*',['img']);
 })
+
